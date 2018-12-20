b44e3b96d38583934baabc00d787a7ad62c1d8d3
Nitin Nithilesh <35731666+NitinNithilesh@users.noreply.github.com>
2018-12-20 22:50:28 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -3,23 +3,44 @@ var scr_config = require('./scr_config.json');
 
 function MessageHandler(context, event) { 
   	var a=event.message;
-	if(event.message=='hi' || event.message=='hey' || event.message=='hello'){
-		context.sendResponse("Hello, How can I help you");
+	if(a=='startchattingevent' || a=='Startchattingevent'){
+		context.sendResponse("Hii, How can I help you..!!");
 	}
-    if(String(a).indexOf("website")>=0 || String(a).indexOf("this website")>=0){
+	if(event.message=='hi' || event.message=='hey' || event.message=='hello' || a=='Hi' || a=='Hey' || a=='Hello'){
+		context.sendResponse("Hello, How can I help you..!!");
+	}
+    if(String(a).indexOf("website")>=0 || String(a).indexOf("this website")>=0 || String(a).indexOf("Website")>=0 || String(a).indexOf("this Website")>=0){
     	context.sendResponse("It is a platform for early education, It Manages classrooms, observe students, collect tuition, and stay in touch with families – all from one easy-to-use childcare app.\nFor more information you can contact us at soldertrust@gmail.com");
     }
-    if(String(a).indexOf("benifits")>=0 || String(a).indexOf("benifit")>=0 || String(a).indexOf("beneficiary")>=0){
+    if(String(a).indexOf("benifits")>=0 || String(a).indexOf("benifit")>=0 || String(a).indexOf("beneficiary")>=0 || String(a).indexOf("Benifits")>=0 || String(a).indexOf("Benifit")>=0 || String(a).indexOf("Beneficiary")>=0){
     	context.sendResponse("85% of users would recommend soldertrust to a director, teacher, or parent.\n100% of parents are more satisfied with their school experience once soldertrust is implemented.\n10s of millions of classroom moments documented and shared per week.\nFor more information you can contact us at soldertrust@gmail.com");
     }
-    if(String(a).indexOf("price")>=0 || String(a).indexOf("pricing")>=0){
+    if(String(a).indexOf("price")>=0 || String(a).indexOf("pricing")>=0 || String(a).indexOf("Price")>=0 || String(a).indexOf("Pricing")>=0){
     	context.sendResponse("Try it free for 30 days. You can view the pricing in the link given below.\nFor more information you can contact us at soldertrust@gmail.com");
     }
-    if(String(a).indexOf("worth")>=0){
+    if(String(a).indexOf("worth")>=0 || String(a).indexOf("Worth")>=0){
     	context.sendResponse("Yes of course\nIt has a award winning support\nIt is Safe & secure\nAnd requires no training");
     }
+    if(String(a).indexOf("help")>=0 || String(a).indexOf("Help")>=0 || String(a).indexOf("get help")>=0 || String(a).indexOf("get Help")>=0 || String(a).indexOf("need help")>=0 || String(a).indexOf("need Help")>=0){
+    	context.sendResponse("Please ask me the question for basic details, I will try my best to answer it. For furthur details please contact soldertrust@gmail.com");
+    }
+    if(String(a).indexOf("signup")>=0 || String(a).indexOf("Signup")>=0){
+    	context.sendResponse("The URL of signup page will be given here after it has been developed");
+    }
+    if(String(a).indexOf("signin")>=0 || String(a).indexOf("Signin")>=0){
+    	context.sendResponse("The URL of signin page will be given here after it has been developed");
+    }
+    if(String(a).indexOf("company")>=0 || String(a).indexOf("Company")>=0 || String(a).indexOf("your company")>=0 || String(a).indexOf("your Company")>=0){
+    	context.sendResponse("It is a platform for early education, It Manages classrooms, observe students, collect tuition, and stay in touch with families – all from one easy-to-use childcare app.\nFor more information you can contact us at soldertrust@gmail.com");	
+    }
+    if(String(a).indexOf("policy")>=0 || String(a).indexOf("Policy")>=0 || String(a).indexOf("policies")>=0 || String(a).indexOf("Policies")>=0 || String(a).indexOf("your policies")>=0 || String(a).indexOf("your Policies")>=0){
+    	context.sendResponse("Under Development, It will be updated once developed");
+    }
+    if(String(a).indexOf("thankyou")>=0 || String(a).indexOf("Thankyou")>=0){
+        context.sendResponse("You're Welcome");
+    }
     else { 
-        context.sendResponse('No keyword found : '+event.message); 
+        context.sendResponse('Sorry I Dont Understand : '+event.message); 
     }
 }
 
